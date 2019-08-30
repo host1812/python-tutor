dc3ae7461a72e86666d56dfa8cf26ce256f91c09
Alexandr Fedorenchik <afedorenchik@apple.com>
2019-08-30 07:13:06 -0700

    poker works now

--- poker/Deck.py
+++ poker/Deck.py
@@ -1,4 +1,5 @@
 from Card import Card
+from random import shuffle as rshuffle
 
 class Deck:
     def __init__(self):
@@ -12,24 +13,47 @@ class Deck:
             raise ValueError("All cards have been dealt")
         
         result = []
-        if n < len(self.cards):
-            for i in range(n):
-                result.append(self.cards.pop())
+        cards_to_deal = min(n,len(self.cards))
+        for i in range(cards_to_deal):
+            result.append(self.cards.pop())
         return result
 
     def deal_card(self):
         return self._deal()
 
+    def shuffle(self):
+        if len(self.cards) < 52:
+            raise ValueError("Only full decks can be shuffled")
+        rshuffle(self.cards)
+
+    def deal_hand(self, n):
+        return self._deal(n)
+
     def __repr__(self):
         return f"Deck of {len(self.cards)} cards"
 
 deck = Deck()
-
 print(deck.cards)
-print(deck)
-print(deck.count())
+deck.shuffle()
+print(deck.cards)
+# print(deck.cards)
+# print(deck)
+# print(deck.count())
 
-print(deck.deal_card())
-print(deck.deal_card())
-print(deck.deal_card())
-print(deck.count())
+# print(deck.deal_card())
+# deck.shuffle()
+# print(deck.deal_card())
+# print(deck.deal_card())
+# print(deck.count())
+
+# print(deck.deal_hand(15))
+# print(deck.count())
+
+# print(deck.deal_hand(25))
+# print(deck.count())
+
+# print(deck.deal_hand(5))
+# print(deck.count())
+
+# print(deck.deal_hand(9))
+# print(deck.count())
---
+++ poker/test_slice1.py
@@ -0,0 +1,39 @@
+# cards = [(s,v) for s in ("Hearts", "Diamonds", "Clubs", "Spades") for v in ("A","J","Q","K","2","3","4","5","6","7","8","9","10")]
+cards = [(s,v) for s in ("Hearts", "Diamonds", "Clubs", "Spades") for v in range(100000)]
+cards2 = [(s,v) for s in ("Hearts", "Diamonds", "Clubs", "Spades") for v in range(100000)]
+
+def get_cards(n):
+    result = []
+    if n <= len(cards):
+            for i in range(n):
+                result.append(cards.pop())
+    return result
+
+def get_cards2(n):
+    global cards2
+    result = []
+    if n <= len(cards2):
+        # print(f"DEBUG: before {cards2}")
+        # import pdb ; pdb.set_trace()
+        result = cards2[len(cards2):-n-1:-1]
+        cards2 = cards2[:len(cards2)-n]
+        # print(f"DEBUG: after {cards2}")
+    return result
+
+# print(get_cards(52))
+# print(f"cards have {len(cards)} left.")
+
+# print(get_cards2(1))
+# print(f"cards2 have {len(cards2)} left.")
+# print(get_cards2(1))
+# print(f"cards2 have {len(cards2)} left.")
+# print(get_cards2(1))
+# print(f"cards2 have {len(cards2)} left.")
+
+for n in range(1000):
+    get_cards2(52)
+
+# if __name__ == '__main__':
+#     import timeit
+#     print(timeit.timeit("get_cards(52)", setup="from __main__ import get_cards"))
+#     print(timeit.timeit("get_cards2(52)", setup="from __main__ import get_cards, get_cards2"))
